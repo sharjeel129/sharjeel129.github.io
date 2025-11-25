d74ccd22f911564c6100a619d2823ebb6fbc668c
sharjeel129 <ahmad.sharjeel129@gmail.com>
2025-11-26 00:44:56 +0500

    fix

--- js/code-viewer.js
+++ js/code-viewer.js
@@ -91,21 +91,78 @@ class CodeViewer {
 	highlightCode(codeBlock) {
 		// Python keywords
 		const keywords = [
-			"def", "class", "import", "from", "if", "else", "elif", "for", "while", "try", 
-			"except", "finally", "with", "return", "yield", "lambda", "pass", "break", 
-			"continue", "in", "is", "not", "and", "or", "as", "None", "True", "False", 
-			"async", "await", "assert", "del", "raise", "global", "nonlocal"
+			"def",
+			"class",
+			"import",
+			"from",
+			"if",
+			"else",
+			"elif",
+			"for",
+			"while",
+			"try",
+			"except",
+			"finally",
+			"with",
+			"return",
+			"yield",
+			"lambda",
+			"pass",
+			"break",
+			"continue",
+			"in",
+			"is",
+			"not",
+			"and",
+			"or",
+			"as",
+			"None",
+			"True",
+			"False",
+			"async",
+			"await",
+			"assert",
+			"del",
+			"raise",
+			"global",
+			"nonlocal",
 		]
-		
+
 		const builtins = [
-			"print", "len", "range", "enumerate", "zip", "map", "filter", "sorted", "sum", 
-			"max", "min", "abs", "str", "int", "float", "list", "dict", "set", "tuple", 
-			"open", "input", "isinstance", "type", "hasattr", "getattr", "setattr", 
-			"super", "property", "staticmethod", "classmethod"
+			"print",
+			"len",
+			"range",
+			"enumerate",
+			"zip",
+			"map",
+			"filter",
+			"sorted",
+			"sum",
+			"max",
+			"min",
+			"abs",
+			"str",
+			"int",
+			"float",
+			"list",
+			"dict",
+			"set",
+			"tuple",
+			"open",
+			"input",
+			"isinstance",
+			"type",
+			"hasattr",
+			"getattr",
+			"setattr",
+			"super",
+			"property",
+			"staticmethod",
+			"classmethod",
 		]
 
 		let text = codeBlock.textContent
-		
+
 		// Escape HTML first
 		text = text
 			.replace(/&/g, "&amp;")
@@ -116,40 +173,46 @@ class CodeViewer {
 		let lines = text.split("\n")
 		let result = []
 
-		lines.forEach(line => {
+		lines.forEach((line) => {
 			let processedLine = line
-			
+
 			// Handle comments
 			let commentIndex = processedLine.indexOf("#")
 			let beforeComment = ""
 			let comment = ""
-			
+
 			if (commentIndex !== -1) {
 				beforeComment = processedLine.substring(0, commentIndex)
 				comment = processedLine.substring(commentIndex)
 			} else {
 				beforeComment = processedLine
 			}
-			
+
 			// Process keywords and builtins in the non-comment part
-			keywords.forEach(keyword => {
+			keywords.forEach((keyword) => {
 				const regex = new RegExp(`\\b${keyword}\\b`, "g")
-				beforeComment = beforeComment.replace(regex, `<span class="py-keyword">${keyword}</span>`)
+				beforeComment = beforeComment.replace(
+					regex,
+					`<span class="py-keyword">${keyword}</span>`
+				)
 			})
-			
-			builtins.forEach(builtin => {
+
+			builtins.forEach((builtin) => {
 				const regex = new RegExp(`\\b${builtin}\\b`, "g")
-				beforeComment = beforeComment.replace(regex, `<span class="py-builtin">${builtin}</span>`)
+				beforeComment = beforeComment.replace(
+					regex,
+					`<span class="py-builtin">${builtin}</span>`
+				)
 			})
-			
+
 			// Add comment styling if present
 			if (comment) {
 				comment = `<span class="py-comment">${comment}</span>`
 			}
-			
+
 			result.push(beforeComment + comment)
 		})
-		
+
 		codeBlock.innerHTML = result.join("\n")
 	}
 }
